c90eca595c1b683a6a1feea25839798dcb86e96f
Milada Borrow <146729026+Miladelka@users.noreply.github.com>
2023-11-26 19:35:52 +0100

    Add files via upload

---
+++ Prikaz_1_cely.sql
@@ -0,0 +1,33 @@
+-- Tento příkaz by měl dát výsledek všech řádků s nejmenším počtem dohromady, ale nejde spustit 
+
+SELECT
+    r.admin_serial,
+    SUM(DATEDIFF(DAY, r.date_request, v.date_borrow)) AS DayDifference
+FROM vypujcky v
+JOIN rezervace_fixed r ON v.Id_No = r.Id_No
+    AND v.admin_serial = r.admin_serial
+    AND r.availability LIKE N'nedostupne'
+    AND DATEDIFF(DAY, r.date_request, v.date_borrow) >= 0
+WHERE v.Id_No IN (SELECT Id_No FROM rezervace_fixed)
+    AND v.admin_serial IN (SELECT admin_serial FROM rezervace_fixed)
+    AND v.date_borrow = (
+        SELECT MIN(date_borrow)
+        FROM vypujcky v2
+        WHERE v2.Id_No = v.Id_No
+        AND v2.admin_serial = v.admin_serial
+        AND DATEDIFF(DAY, r.date_request, v2.date_borrow) >= 0
+    )
+    AND NOT EXISTS (
+        SELECT
+            *
+        FROM
+            rezervace_fixed
+        WHERE
+            admin_serial = r.admin_serial
+            AND r.Id_No = Id_No
+            AND date_request > r.date_request
+            AND date_request < v.date_borrow)
+GROUP BY
+    r.admin_serial
+ORDER BY
+    DayDifference DESC
